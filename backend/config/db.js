d7ff4fb600703cdbcc02cb55a6a756db7e92a4bc
Ishraq3002 <91436384+Ishraq3002@users.noreply.github.com>
2024-02-03 23:30:43 -0500

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URL, {
+        });
+
+        console.log(`MongoDB Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit();
+    }
+};
+
+module.exports = connectDB;
